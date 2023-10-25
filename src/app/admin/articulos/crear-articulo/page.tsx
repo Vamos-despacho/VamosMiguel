f6eb506214620aab5f03ade558101c437a3e38ef
christhianjpp <christhianjpp@gmil.com>
2023-10-25 14:11:03 -0500

    if etiquetas categorias0

--- src/app/admin/articulos/crear-articulo/page.tsx
+++ src/app/admin/articulos/crear-articulo/page.tsx
@@ -14,7 +14,8 @@ const CrearArticle = async () => {
     const categorias = await getCategorias()
     const etiquetas = await getEtiquetas()
 
-
+    if (!categorias) return <div>No hay categorías</div>
+    if (!etiquetas) return <div>No hay etiquetas</div>
 
 
     return (
