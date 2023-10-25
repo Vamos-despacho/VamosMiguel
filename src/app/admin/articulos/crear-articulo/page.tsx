32ed3242c073d28a0fba696a86e65efac15eea8b
christhianjpp <christhianjpp@gmil.com>
2023-10-25 14:23:12 -0500

    if etiquetas categorias2

--- src/app/admin/articulos/crear-articulo/page.tsx
+++ src/app/admin/articulos/crear-articulo/page.tsx
@@ -5,25 +5,20 @@ async function getCategorias() {
     return await prisma.category.findMany()
 }
 async function getEtiquetas() {
-
     return await prisma.tag.findMany()
 }
 const CrearArticle = async () => {
 
-    const categorias = await getCategorias()
-    const etiquetas = await getEtiquetas()
-    console.log(categorias)
-    console.log(etiquetas)
+    // const categorias = await getCategorias()
+    // const etiquetas = await getEtiquetas()
 
-    if (!categorias) return <div>No hay categorías</div>
-    if (!etiquetas) return <div>No hay etiquetas</div>
 
     return (
         <div>
-            <FormtArticle
+            {/* <FormtArticle
                 category={categorias}
                 tags={etiquetas}
-            />
+            /> */}
         </div>
     )
 }
