1812b106f921bca211ad77320c38af5400c2ddad
christhianjpp <christhianjpp@gmil.com>
2024-08-10 11:10:57 -0500

    register error

--- src/actions/events/register.ts
+++ src/actions/events/register.ts
@@ -1,16 +1,16 @@
-"use server"
-import connectDB from "@/lib/mongodb";
-import Events from "@/models/Events";
+// "use server"
+// import connectDB from "@/lib/mongodb";
+// import Events from "@/models/Events";
 
-export const register = async (values: any) => {
-    console.log(values)
-    try {
-        await connectDB();
-        const event = new Events(values);
-        const result = await event.save();
-        return result;
-    } catch (error) {
-        console.error(error);
-        return Promise.reject(error);
-    }
-}
+// export const register = async (values: any) => {
+//     console.log(values)
+//     try {
+//         await connectDB();
+//         const event = new Events(values);
+//         const result = await event.save();
+//         return result;
+//     } catch (error) {
+//         console.error(error);
+//         return Promise.reject(error);
+//     }
+// }
