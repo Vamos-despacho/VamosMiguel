e5a6e97b2bf31f3730e09abd6224b6199f24ef21
christhianjpp <christhianjpp@gmil.com>
2024-08-23 11:15:11 -0500

    actividades0

--- src/app/api/vamosApi.tsx
+++ src/app/api/vamosApi.tsx
@@ -4,9 +4,9 @@ import axios from "axios";
 
 
 // const baseURL = 'http://192.168.0.3:8000/api';
-// const baseURL = 'https://vamos-miguel-angel.vercel.app/api';
+const baseURL = 'https://vamos-miguel-angel.vercel.app/api';
 // const baseURL = '/api';
-const baseURL = 'http://localhost:3000/api';
+// const baseURL = 'http://localhost:3000/api';
 
 const vamosApi = axios.create({ baseURL })
 
