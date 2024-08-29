c1f04c813e4ad3d498e90db33101f9fb09470c02
christhianjpp <christhianjpp@gmil.com>
2024-08-29 09:35:29 -0500

    day2

--- src/app/data/article.js
+++ src/app/data/article.js
@@ -3,7 +3,7 @@ import { Category } from "@/interface/category"
 
 export const articlesData = [
     {
-        "id": 6,
+        "id": '6',
         "title": "Visita a la Ciudad de la Salud",
         "content": `<div className="prose mx-auto mt-4 max-w-3xl space-y-4">
   <p class="text-justify">Durante nuestra visita a la Ciudad de la Salud, recorrimos diversas áreas especializadas del centro hospitalario. Observamos el trabajo que realiza el personal médico en la atención a los pacientes y conocimos los recursos disponibles para ofrecer servicios de salud.</p>
@@ -19,7 +19,7 @@ export const articlesData = [
         "published": true,
     },
     {
-        "id": 5,
+        "id": '5',
         "title": "Reunión con el Ministro de Salud para Mejorar la Atención en Veraguas",
         "content": `<div className="prose mx-auto mt-4 max-w-3xl space-y-4">
   <p class="text-justify">Mantuvimos una reunión con el Ministro de Salud, Fernando Boyd, en la cual expusimos las urgentes necesidades de la provincia de Veraguas.</p>
@@ -36,7 +36,7 @@ export const articlesData = [
         "published": true,
     },
     {
-        "id": 4,
+        "id": '4',
         "title": "Reunión con el Director de CSS",
         "content": `<div className="prose mx-auto mt-4 max-w-3xl space-y-4">
   <p class="text-justify">Nos reunimos para analizar la situación de la Caja de Seguro Social (CSS), y constatamos que enfrenta desafíos significativos en términos de sostenibilidad financiera y eficiencia operativa. Observamos un aumento en la demanda de servicios de salud y pensiones, especialmente debido al envejecimiento de la población y a los cambios demográficos. Estos factores ejercen una creciente presión sobre los recursos de la CSS.</p>
@@ -52,7 +52,7 @@ export const articlesData = [
         "published": true,
     },
     {
-        "id": 3,
+        "id": '3',
         "title": "Primer Anteproyecto Presentado",
         "content": `<div className="prose mx-auto mt-4 max-w-3xl space-y-4">
   <p class="text-justify">Se presentó ante la primera sesión ordinaria del pleno de la Asamblea el primer anteproyecto de ley, que modifica la Ley 1 del 11 de enero de 1965. Esta modificación prohíbe el acceso a auxilios económicos a funcionarios de altos mandos y jurisdicción, así como a sus familiares hasta el cuarto grado de consanguinidad y el segundo de afinidad.</p>
@@ -69,7 +69,7 @@ export const articlesData = [
         "published": true,
     },
     {
-        "id": 2,
+        "id": '2',
         "title": "Entrega de Credenciales",
         "content": `<div className="prose mx-auto mt-4 max-w-3xl space-y-4">
   <p class="text-justify">Recibimos las credenciales por parte del Tribunal Electoral (TE), acreditándonos como diputados de la República luego de las elecciones del 5 de mayo, para el periodo constitucional 2024-2029. Las credenciales fueron entregadas el viernes 21 de junio de 2024 durante un acto celebrado en la sede principal del TE, presidido por los magistrados en Pleno.</p>
@@ -87,7 +87,7 @@ export const articlesData = [
         "published": true,
     },
     {
-        "id": 1,
+        "id": '1',
         "title": "La importancia del voto de los jóvenes en este 2024",
         "content": "<p class=\"ql-align-justify\">A escasos meses para las elecciones, me he detenido a pensar que nos encontramos ante un ciclo electoral crucial: elegir a nuestros futuros gobernantes. Este hecho marcará el futuro de nuestro país los próximos 5 años.</p><p class=\"ql-align-justify\"><br></p><p class=\"ql-align-justify\">Panamá se encuentra en una encrucijada histórica, marcada por estallidos sociales significativos y un protagonismo emergente de la juventud como catalizador de cambio. En los últimos meses, hemos sido testigos de los movimientos sociales que han sacudido las calles de nuestro país. El descontento con las políticas públicas gubernamentales, corrupción percibida y una desigualdad que crece a pasos agigantados han obligado a la juventud a movilizarse. Todo esto no es nada ajeno al clamor de reformas educativas, laborales, transparencia, rendición de cuentas y a una verdadera participación ciudadana.</p><p class=\"ql-align-justify\"><br></p><p class=\"ql-align-justify\">Nuestro país enfrenta desafíos económicos y sociales que impactan de forma directa a los jóvenes, alta tasa de desempleo juvenil, acceso limitado a una educación de calidad y una creciente sensación de exclusión política, lo que ha encendido esa chispa de lucha y protesta en el corazón de la juventud.</p><p class=\"ql-align-justify\"><br></p><p class=\"ql-align-justify\">A medida que las elecciones de 2024 se acercan, la pregunta del peso del voto joven cobra una alta relevancia. Con cerca del 20% de la población joven habilitada para ejercer su voto, la influencia de este porcentaje sobre el resultado electoral es indiscutible. Sin embargo, más allá de la mera estadística, el voto joven trae consigo una real promesa de renovación y cambio. Es una generación más informada, conectada y consciente de sus derechos y responsabilidades cívicas.</p><p class=\"ql-align-justify\"><br></p><p class=\"ql-align-justify\">El camino hacia las urnas no se encuentra exento de obstáculos para los jóvenes panameños. La desilusión política, la falta de representatividad y las barreras para el acceso a la información son solo algunos de los retos a superar. Pero, estos desafíos también presentan oportunidades para el surgimiento de nuevas plataformas de participación, el cuestionamiento de narrativas tradicionales y el potencial de forjar un amanecer distinto para el país.</p><p class=\"ql-align-justify\"><br></p><p class=\"ql-align-justify\">Los jóvenes no solo somos un número en las próximas elecciones; somos una voz, un símbolo de esperanza y un agente de cambio. En la medida en que nos acercamos a un momento histórico en nuestra democracia, el poder de los jóvenes para influir en el curso del país es más evidente que nunca. Nuestro desafío está en no solo reconocer, sino poder catalizarlo hacia la construcción de un Panamá más justo y próspero. El futuro de Panamá está, en gran medida, en las manos de su juventud, y su voto es la llave para el cambio que tanto anhelan ver.</p><p class=\"ql-align-justify\"><br></p><p class=\"ql-align-justify\">La situación de Panamá es un llamado a la acción para todos, pero especialmente para los jóvenes. A cuatro meses de las elecciones, la expectativa es alta, y el margen para influir, amplio. El voto joven no solo puede ser determinante en 2024: puede ser el comienzo de una nueva era para Panamá.</p>",
         "imageUrl": "https://res.cloudinary.com/dl6oea68u/image/upload/v1704659900/VamosMA/nbqz14zjoktr1boa8rwi.avif",
