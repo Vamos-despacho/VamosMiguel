db3594170e7262a69c30c07c6240b6c0cc368a0b
christhianjpp <christhianjpp@gmil.com>
2024-11-01 12:42:53 -0500

    ley sansinada

--- src/app/api/data/anteproyectos.tsx
+++ src/app/api/data/anteproyectos.tsx
@@ -1,62 +1,63 @@
-import { Category } from "@/interface/category"
-import { LawProposalState } from "@/interface/states"
+import { Category } from "@/interface/category";
+import { LawProposalState } from "@/interface/states";
 
 export interface IAnteProyecto {
-    id: number
-    titulo: string
-    descripcion: string
-    fecha: string
-    categoria: string
-    proponente: string,
-    icon: string,
-    link: string
-    estado?: LawProposalState
+  id: number;
+  titulo: string;
+  descripcion: string;
+  fecha: string;
+  categoria: string;
+  proponente: string;
+  icon: string;
+  link: string;
+  estado?: LawProposalState;
 }
 
 export const Anteproyectos: IAnteProyecto[] = [
-    {
-        id: 3,
-        titulo: 'Reorganización del Instituto de Deportes y Normativas Antidopaje',
-        // titulo: 'Reforma Integral para la Transparencia y Equidad en la Asignación de Auxilios Económicos',
-        // titulo:'Que modifica la Ley 1 del 11 de enero de 1965 y prohíbe el acceso a Auxilios Económicos a funcionarios de Alto Mando y Jurisdicción ya sus familiares con parentesco hasta el cuarto grado de consanguinidad y segundo de afinidad',
+  {
+    id: 3,
+    titulo: "Reorganización del Instituto de Deportes y Normativas Antidopaje",
+    // titulo: 'Reforma Integral para la Transparencia y Equidad en la Asignación de Auxilios Económicos',
+    // titulo:'Que modifica la Ley 1 del 11 de enero de 1965 y prohíbe el acceso a Auxilios Económicos a funcionarios de Alto Mando y Jurisdicción ya sus familiares con parentesco hasta el cuarto grado de consanguinidad y segundo de afinidad',
 
-        descripcion: 'Este anteproyecto modifica la Ley 16 de 1995 para reorganizar el Instituto Nacional de Deportes, en los artículos 15K, 15L, y 15M para asegurar el cumplimiento de normas antidopaje según la ONAD-PAN y el código mundial antidopaje vigente.',
-        fecha: 'Tue Aug 14 2024 22:12:41 GMT-0500 (hora estándar oriental)',
-        categoria: Category.Educacion,
-        proponente: 'H.D. Miguel Ángel Campos Lima',
-        icon: 'MdOutlineSportsSoccer',
-        link: 'pdfs/AnteproyectodeLeyANTIDOPING.pdf',
-        // link: 'https://drive.google.com/file/d/1bb5jZOu8J_d1grqhQDnERynxDFkHvLcR/edit',
-        estado: LawProposalState.EnEsperaDeSanacionPorElEjecutivo
-    },
-    {
-        id: 2,
-        titulo: 'Alto Rendimiento Deportivo y Medicina Deportiva',
-        // titulo: 'Reforma Integral para la Transparencia y Equidad en la Asignación de Auxilios Económicos',
-        // titulo:'Que modifica la Ley 1 del 11 de enero de 1965 y prohíbe el acceso a Auxilios Económicos a funcionarios de Alto Mando y Jurisdicción ya sus familiares con parentesco hasta el cuarto grado de consanguinidad y segundo de afinidad',
+    descripcion:
+      "Este anteproyecto modifica la Ley 16 de 1995 para reorganizar el Instituto Nacional de Deportes, en los artículos 15K, 15L, y 15M para asegurar el cumplimiento de normas antidopaje según la ONAD-PAN y el código mundial antidopaje vigente.",
+    fecha: "Tue Aug 14 2024 22:12:41 GMT-0500 (hora estándar oriental)",
+    categoria: Category.Educacion,
+    proponente: "H.D. Miguel Ángel Campos Lima",
+    icon: "MdOutlineSportsSoccer",
+    link: "pdfs/AnteproyectodeLeyANTIDOPING.pdf",
+    // link: 'https://drive.google.com/file/d/1bb5jZOu8J_d1grqhQDnERynxDFkHvLcR/edit',
+    estado: LawProposalState.LeySancionada,
+  },
+  {
+    id: 2,
+    titulo: "Alto Rendimiento Deportivo y Medicina Deportiva",
+    // titulo: 'Reforma Integral para la Transparencia y Equidad en la Asignación de Auxilios Económicos',
+    // titulo:'Que modifica la Ley 1 del 11 de enero de 1965 y prohíbe el acceso a Auxilios Económicos a funcionarios de Alto Mando y Jurisdicción ya sus familiares con parentesco hasta el cuarto grado de consanguinidad y segundo de afinidad',
 
-        descripcion: 'Proporciona los recursos necesarios para mejorar el nivel competitivo de nuestros atletas, implementar la medicina deportiva preventiva, y hacer del deporte una cultura y una industria.',
-        fecha: 'Tue Aug 06 2024 22:12:41 GMT-0500 (hora estándar oriental)',
-        categoria: Category.Educacion,
-        proponente: 'H.D. Miguel Ángel Campos Lima',
-        icon: 'MdOutlineSportsSoccer',
-        link: 'pdfs/AnteproyectodeAltoRendimientoDeportivoyMedicinaDeportiva.pdf',
-        estado: LawProposalState.Preliminario
-    },
-    {
-        id: 1,
-        titulo: 'Transparencia y Equidad en Auxilios Económicos',
-        // titulo: 'Reforma Integral para la Transparencia y Equidad en la Asignación de Auxilios Económicos',
-        // titulo:'Que modifica la Ley 1 del 11 de enero de 1965 y prohíbe el acceso a Auxilios Económicos a funcionarios de Alto Mando y Jurisdicción ya sus familiares con parentesco hasta el cuarto grado de consanguinidad y segundo de afinidad',
+    descripcion:
+      "Proporciona los recursos necesarios para mejorar el nivel competitivo de nuestros atletas, implementar la medicina deportiva preventiva, y hacer del deporte una cultura y una industria.",
+    fecha: "Tue Aug 06 2024 22:12:41 GMT-0500 (hora estándar oriental)",
+    categoria: Category.Educacion,
+    proponente: "H.D. Miguel Ángel Campos Lima",
+    icon: "MdOutlineSportsSoccer",
+    link: "pdfs/AnteproyectodeAltoRendimientoDeportivoyMedicinaDeportiva.pdf",
+    estado: LawProposalState.Preliminario,
+  },
+  {
+    id: 1,
+    titulo: "Transparencia y Equidad en Auxilios Económicos",
+    // titulo: 'Reforma Integral para la Transparencia y Equidad en la Asignación de Auxilios Económicos',
+    // titulo:'Que modifica la Ley 1 del 11 de enero de 1965 y prohíbe el acceso a Auxilios Económicos a funcionarios de Alto Mando y Jurisdicción ya sus familiares con parentesco hasta el cuarto grado de consanguinidad y segundo de afinidad',
 
-        descripcion: 'La propuesta prohíbe el acceso a auxilios económicos a funcionarios de alto mando y sus familiares hasta el cuarto grado de consanguinidad y segundo de afinidad, promoviendo la transparencia y equidad en la distribución de recursos educativos.',
-        fecha: 'Tue Jul 02 2024 22:12:41 GMT-0500 (hora estándar oriental)',
-        categoria: Category.Educacion,
-        proponente: 'H.D. Miguel Ángel Campos Lima',
-        icon: 'SlBookOpen',
-        link: 'pdfs/ReformaparalaTransparenciayEquidadenAuxiliosEconomicos.pdf',
-        estado: LawProposalState.Subcomision
-    },
-
-
-]
+    descripcion:
+      "La propuesta prohíbe el acceso a auxilios económicos a funcionarios de alto mando y sus familiares hasta el cuarto grado de consanguinidad y segundo de afinidad, promoviendo la transparencia y equidad en la distribución de recursos educativos.",
+    fecha: "Tue Jul 02 2024 22:12:41 GMT-0500 (hora estándar oriental)",
+    categoria: Category.Educacion,
+    proponente: "H.D. Miguel Ángel Campos Lima",
+    icon: "SlBookOpen",
+    link: "pdfs/ReformaparalaTransparenciayEquidadenAuxiliosEconomicos.pdf",
+    estado: LawProposalState.Subcomision,
+  },
+];
--- src/app/informe-de-gestion/page.tsx
+++ src/app/informe-de-gestion/page.tsx
@@ -1,82 +1,80 @@
-
-import AccordionCardList from '@/components/informes/AccordioncardList';
-import GraficoBar from '@/components/informes/GraficoBar'
-import SliderImage from '@/components/SliderImage';
+import AccordionCardList from "@/components/informes/AccordioncardList";
+import GraficoBar from "@/components/informes/GraficoBar";
+import SliderImage from "@/components/SliderImage";
 
 const listItems = [
-    {
-        id: '1',
-        link: 'pdfs/AnteproyectodeLeyANTIDOPING.pdf',
-        estado: 'En espera de sanción por el Ejecutivo',
-        title: 'Reorganización del Instituto de Deportes y Normativas Antidopaje',
-        content: 'Este anteproyecto modifica la Ley 16 de 1995 para reorganizar el Instituto Nacional de Deportes, en los artículos 15K, 15L, y 15M para asegurar el cumplimiento de normas antidopaje según la ONAD-PAN y el código mundial antidopaje vigente.',
-
-    },
-    {
-        id: '2',
-        link: 'pdfs/AnteproyectodeAltoRendimientoDeportivoyMedicinaDeportiva.pdf',
-        estado: 'Preliminar',
-        title: 'Alto Rendimiento Deportivo y Medicina Deportiva',
-        content: 'Proporciona los recursos necesarios para mejorar el nivel competitivo de nuestros atletas, implementar la medicina deportiva preventiva, y hacer del deporte una cultura y una industria.',
-
-    },
-    {
-        id: '3',
-        link: 'pdfs/ReformaparalaTransparenciayEquidadenAuxiliosEconomicos.pdf',
-        estado: 'Subcomisión',
-        title: 'Transparencia y Equidad en Auxilios Económicos',
-        content: 'La propuesta prohíbe el acceso a auxilios económicos a funcionarios de alto mando y sus familiares hasta el cuarto grado de consanguinidad y segundo de afinidad, promoviendo la transparencia y equidad en la distribución de recursos educativos.',
-
-    },
+  {
+    id: "1",
+    link: "pdfs/AnteproyectodeLeyANTIDOPING.pdf",
+    estado: "Ley Sancionada",
+    title: "Reorganización del Instituto de Deportes y Normativas Antidopaje",
+    content:
+      "Este anteproyecto modifica la Ley 16 de 1995 para reorganizar el Instituto Nacional de Deportes, en los artículos 15K, 15L, y 15M para asegurar el cumplimiento de normas antidopaje según la ONAD-PAN y el código mundial antidopaje vigente.",
+  },
+  {
+    id: "2",
+    link: "pdfs/AnteproyectodeAltoRendimientoDeportivoyMedicinaDeportiva.pdf",
+    estado: "Preliminar",
+    title: "Alto Rendimiento Deportivo y Medicina Deportiva",
+    content:
+      "Proporciona los recursos necesarios para mejorar el nivel competitivo de nuestros atletas, implementar la medicina deportiva preventiva, y hacer del deporte una cultura y una industria.",
+  },
+  {
+    id: "3",
+    link: "pdfs/ReformaparalaTransparenciayEquidadenAuxiliosEconomicos.pdf",
+    estado: "Subcomisión",
+    title: "Transparencia y Equidad en Auxilios Económicos",
+    content:
+      "La propuesta prohíbe el acceso a auxilios económicos a funcionarios de alto mando y sus familiares hasta el cuarto grado de consanguinidad y segundo de afinidad, promoviendo la transparencia y equidad en la distribución de recursos educativos.",
+  },
 ];
 
-
 const page = () => {
-    return (
-        <section className="mx-auto max-w-7xl px-0 sm:px-6 lg:px-0 py-10  lg:py-16 h-full">
-            <div className="sm:container">
-                {/* Título y Subtítulo */}
-                <div className="mx-auto max-w-4xl text-center">
-                    <h2 className="text-3xl md:text-5xl font-bold leading-tight">
-                        Informe de Gestión
-                    </h2>
-                    <p className="mt-2 sm:mt-4 text-lg text-gray-600">
-                        Transparencia y compromiso en cada acción legislativa, reflejando el esfuerzo constante por el bienestar de la comunidad y el desarrollo del país.
-                    </p>
-                </div>
-                {/* Primeros 100 días de gestión */}
-                <div className="mt-10 px-3 sm:mt-16">
-                    <div className="flex flex-col  justify-center md:justify-start">
-                        <h3 className="text-2xl  font-semibold">
-                            Primeros 100 días de gestión
-                        </h3>
-                        <span className="text-sm text-gray-500 font-medium ml-2">
-                            Julio 2024 - Octubre 2024
-                        </span>
-                    </div>
+  return (
+    <section className="mx-auto max-w-7xl px-0 sm:px-6 lg:px-0 py-10  lg:py-16 h-full">
+      <div className="sm:container">
+        {/* Título y Subtítulo */}
+        <div className="mx-auto max-w-4xl text-center">
+          <h2 className="text-3xl md:text-5xl font-bold leading-tight">
+            Informe de Gestión
+          </h2>
+          <p className="mt-2 sm:mt-4 text-lg text-gray-600">
+            Transparencia y compromiso en cada acción legislativa, reflejando el
+            esfuerzo constante por el bienestar de la comunidad y el desarrollo
+            del país.
+          </p>
+        </div>
+        {/* Primeros 100 días de gestión */}
+        <div className="mt-10 px-3 sm:mt-16">
+          <div className="flex flex-col  justify-center md:justify-start">
+            <h3 className="text-2xl  font-semibold">
+              Primeros 100 días de gestión
+            </h3>
+            <span className="text-sm text-gray-500 font-medium ml-2">
+              Julio 2024 - Octubre 2024
+            </span>
+          </div>
 
-                    {/* Gráfico y Lista Acordeón */}
-                    <div className="mt-5 border-t pt-10">
-                        <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
-                            {/* Gráfico */}
-                            <div className="flex flex-col px-2">
-                                <AccordionCardList items={listItems} />
-                            </div>
-                            <div className="flex justify-center items-center">
-                                <GraficoBar />
-                            </div>
+          {/* Gráfico y Lista Acordeón */}
+          <div className="mt-5 border-t pt-10">
+            <div className="grid grid-cols-1 md:grid-cols-2 gap-6">
+              {/* Gráfico */}
+              <div className="flex flex-col px-2">
+                <AccordionCardList items={listItems} />
+              </div>
+              <div className="flex justify-center items-center">
+                <GraficoBar />
+              </div>
 
-                            {/* Lista Acordeón */}
-                        </div>
-                        <SliderImage />
-                    </div>
-                    <div className='border-t mt-12'>
-                    </div>
-                </div>
+              {/* Lista Acordeón */}
             </div>
-        </section>
+            <SliderImage />
+          </div>
+          <div className="border-t mt-12"></div>
+        </div>
+      </div>
+    </section>
+  );
+};
 
-    )
-}
-
-export default page
+export default page;
