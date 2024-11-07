1e27efbc28426265d102ba37634766023a41295a
christhianjpp <christhianjpp@gmil.com>
2024-11-07 14:06:43 -0500

    imagen sesion

--- src/components/leycss/ImagenSesion.tsx
+++ src/components/leycss/ImagenSesion.tsx
@@ -5,12 +5,12 @@ const ImagenSesion = () => {
     <div className="flex flex-col lg:flex-row gap-4 lg:gap-6 justify-center items-center">
       <img
         src="/images/sesion/sesion01.jpeg"
-        className="w-full lg:w-2/5 max-h-[50vh] object-cover rounded-lg "
+        className="w-full lg:w-2/5 max-h-[60vh] object-cover rounded-lg overflow-hidden"
         alt="Imagen Sesión 1"
       />
       <img
         src="/images/sesion/sesion02.jpeg"
-        className="w-full lg:w-2/5 max-h-[50vh] object-cover rounded-lg "
+        className="w-full lg:w-2/5 max-h-[60vh] object-cover rounded-lg overflow-hidden"
         alt="Imagen Sesión 2"
       />
     </div>
