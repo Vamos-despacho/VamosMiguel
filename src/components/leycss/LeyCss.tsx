d45e041dc3fc589e1d6d9bf286d267df005dd7da
christhianjpp <christhianjpp@gmil.com>
2024-11-07 13:51:32 -0500

    link

--- src/components/leycss/LeyCss.tsx
+++ src/components/leycss/LeyCss.tsx
@@ -7,13 +7,18 @@ const link =
   "https://drive.google.com/file/d/1uzLhRXC8LiSxKvSqDWpHvTXX-mI72KMw/preview";
 const LeyCss = () => {
   const [isOpen, setIsOpen] = useState(false);
-
+  const handleDownload = () => {
+    const downloadUrl =
+      "https://drive.google.com/file/d/1uzLhRXC8LiSxKvSqDWpHvTXX-mI72KMw/view";
+    window.open(downloadUrl, "_blank"); // Abre en una nueva pestaña para iniciar la descarga
+  };
   return (
     <div>
       <div className="flex flex-col items-start gap-1">
         <button
           className="flex items-center justify-center"
-          onClick={() => setIsOpen(true)}
+          // onClick={() => setIsOpen(true)}
+          onClick={handleDownload}
         >
           <p className="flex items-center text-base cursor-pointer p-2 hover:underline hover:text-blue-400">
             Ver Proyecto de Ley 51
